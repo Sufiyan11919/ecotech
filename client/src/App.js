b87267203edb744c5fab644191927fe73149db09
sufi <sufiyanshaikh11919@gmail.com>
2023-10-05 18:21:25 -0700

    created react app

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src="" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+        {/* <img src="https://www.bing.com/images/search?view=detailV2&ccid=iSu2RcCc&id=4E59EEBA2E14AC539CBCFC0C01F0751151A844D9&thid=OIP.iSu2RcCcdm78xbxNDJMJSgHaEo&mediaurl=https%3a%2f%2fwww.wallpapers13.com%2fwp-content%2fuploads%2f2015%2f12%2fNature-Lake-Bled.-Desktop-background-image.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.892bb645c09c766efcc5bc4d0c93094a%3frik%3d2USoURF18AEM%252fA%26pid%3dImgRaw%26r%3d0&exph=1600&expw=2560&q=image&simid=608023423875297712&FORM=IRPRST&ck=542186EFD007C2E3206775C5AB4B0111&selectedIndex=0&ajaxhist=0&ajaxserp=0" alt="" /> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
