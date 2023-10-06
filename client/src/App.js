fca119db562ad7eade82859d5ebb2daa6ab65959
sufi <sufiyanshaikh11919@gmail.com>
2023-10-06 00:05:19 -0700

    routes and connected to mongo atlas

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,3 @@
-// import React from 'react';
-// import ReactDom from 'react-dom/client';
-// // import {BrowserRouter as Router, Routes , Route} from 'react-router-dom';
-// import {createBrowserRouter, Router, RouterProvider} from 'react-router-dom'
-// import Home from './components/Home';
-
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         <header>
-//           <h1>My React Router App</h1>
-//         </header>
-//         <main>
-//           <Routes>
-//             <Route exact path="/" component={Home} />
-//             {/* <Home/> */}
-//             <Route path="/plant_detail" component={Home} /> 
-//             {/* <Home/> */}
-//           </Routes>
-//         </main>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
--- server/server.js
+++ server/server.js
@@ -1,11 +1,48 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
+const mongoose = require('mongoose');
+const db = 'mongodb+srv://sufiyanshaikh11919:sE10BaAqd6Swx02u@cluster0.fanty6g.mongodb.net/ecotechDB?retryWrites=true&w=majority'
 
+mongoose.connect(db, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false,
+}).then(()=>{
+    console.log("db connected")
+}).catch((err)=>{
+    console.log(err)
+});
+
+// middleware
+const middleware = (request, response,)=>{
+    console.log("middleware");
+   next();
+}
+ 
 app.get('/',(request, response)=>{
     response.send("hey")
-})
+});
+app.get('/x', middleware, (request, response)=>{
+    console.log("middleware working");
+    response.send("x")
+});
+app.get('/y',(request, response)=>{
+    response.send("y")
+});
+app.get('/z',(request, response)=>{
+    response.send("z")
+});
+app.get('/signin',(request, response)=>{
+    response.send("z")
+});
+app.get('/signup',(request, response)=>{
+    response.send("z")
+});
 
 
 app.listen(5000, ()=>{
     console.log("server connected")
+    console.log("http://localhost:5000/")
 })
