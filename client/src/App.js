d5b7e0f6a556c652a320230412aeb945e67cf049
sufi <sufiyanshaikh11919@gmail.com>
2023-10-06 05:04:49 -0700

    added bunch of files and used bootstrap's navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Navbar from './components/Navbar'
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
+import PlantDetail from './pages/PlantDetail'
+import PlantForm from './pages/PlantForm'
+import PlantProgress from './pages/PlantProgress'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar/>
+        <Home/>
       </header>
     </div>
   );
